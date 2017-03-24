8d69d6ca366e0fe61c0e45e8e8971783a3d3d083
Anna Kimtis <Anna@Annas-MBP.verizon.net>
2017-03-24 14:28:45 -0400

    added helper functions to orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,29 @@
 var connection = require("../config/connection.js");
 
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// Helper function for SQL syntax.
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    if (Object.hasOwnProperty.call(ob, key)) {
+      arr.push(key + "=" + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
+
 // Object for all our SQL statement functions.
 var orm = {
   all: function(tableInput, cb) {
@@ -48,6 +72,20 @@ var orm = {
       cb(result);
     });
   },
+  delete: function(table, condition, cb) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
 };
+
 
   module.exports = orm;
